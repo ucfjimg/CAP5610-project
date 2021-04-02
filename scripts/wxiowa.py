0e6e07aec09a5d7ac1bbe67bcbc797c13618ef53
Jim Geist <jimg@knights.ucf.edu>
2021-04-01 20:24:41 -0400

    More extreme weather HWI CWI

--- scripts/wxiowa.py
+++ scripts/wxiowa.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import pandas as pd
+import numpy as np
 from os import path
 import sys
 
@@ -31,8 +32,8 @@ m0['TMIN'] = m0[['TMIN_x', 'TMIN_y']].mean(axis=1)
 m0['TMAX'] = m0[['TMAX_x', 'TMAX_y']].mean(axis=1)
 temp = m0.drop(columns=['TMIN_x', 'TMIN_y', 'TMAX_x', 'TMAX_y'])
 
-
 data = prcp.merge(temp, on='DATE', how='outer')
+data.sort_values(by='DATE', inplace=True)
 
 # rescaler to mm
 data['PRCP'] = data['PRCP'] * 0.1
@@ -55,26 +56,21 @@ data = data.loc[index:]
 
 data.to_csv(path.join(dataroot, '..', 'wx-frontier-daily.csv'))
 
-# compute some stats
+data['YEAR'] = data.DATE.str[0:4].astype(int)
+data['MONTH'] = data.DATE.str[5:7].astype(int)
+data['DAY'] = data.DATE.str[8:10].astype(int)
+data['CALDAY'] = data.DATE.str[5:10]
 
-def day_in_season(date, year):
-    y, month, day = [int(x) for x in date.split('-')]
-    #
-    # Season is 10-May to 20-Oct
-    #
-    if y != year:
-        return False
-    if month == 5 and day >= 10:
-        return True
-    if month == 10 and day <= 20:
-        return True
-    return month > 5 and month < 10
 
-def in_season(dates, year):
-    return map(lambda x: day_in_season(x, year), dates)
+baseline_years = range(1961, 1991)
+all_years = range(1941, 2021)
 
-years = range(1941, 2021)
-
+#
+# compute percentiles needed for later extreme weather features
+#
+baseline = data.loc[(data.YEAR >= 1961) & (data.YEAR < 1991)]
+tmax90 = baseline[['TMAX', 'CALDAY']].groupby('CALDAY').agg(lambda lst: np.percentile(lst, 90))
+tmin20 = baseline[['TMIN', 'CALDAY']].groupby('CALDAY').agg(lambda lst: np.percentile(lst, 20))
 
 GSP = []
 GDD = []
@@ -82,10 +78,19 @@ GSTmax = []
 GSTmin = []
 frost = []
 summer = []
+HWI = []
+CWI = []
+
+def is_season_day(m, d):
+    '''
+    Returns True if the given date is both in `year` and is in the
+    defined (May 10-Oct 20) growing season.
+    '''
+    return ((m == 5) & (d >= 10)) | ((m >= 6) & (m <= 9)) | ((m == 10) & (d <= 20))
 
 
-for year in years:
-    season = data.loc[in_season(data.DATE, year)]  
+for year in all_years:
+    season = data.loc[(data.YEAR == year) & (is_season_day(data.MONTH, data.DAY))]  
     precip = season.PRCP.mean()
     # GSP: Precipitation averaged over the growing season
     GSP.append(precip)
@@ -104,16 +109,25 @@ for year in years:
     # Summer days - count of days with tmax > 25
     summer.append((season.TMAX > 25).sum())
 
+    tmax = season.merge(tmax90, on='CALDAY')
+    tmax = tmax.TMAX_x > tmax.TMAX_y
+    HWI.append(sum(tmax))
 
-years = pd.Series(years, name='YEAR')
+    tmin = season.merge(tmin20, on='CALDAY')
+    tmin = tmin.TMIN_x < tmin.TMIN_y
+    CWI.append(sum(tmin))
+
+years = pd.Series(all_years, name='YEAR')
 GSP = pd.Series(GSP, name='GSP')
 GDD = pd.Series(GDD, name='GDD')
 GSTmax = pd.Series(GSTmax, name='GSTmax')
 GSTmin = pd.Series(GSTmin, name='GSTmin')
 frost = pd.Series(frost, name='frost')
 summer = pd.Series(summer, name='summer')
+HWI = pd.Series(HWI, name='HWI')
+CWI = pd.Series(CWI, name='CWI')
 
-data = pd.DataFrame([years, GSP, GDD, GSTmin, GSTmax, frost, summer]).T
+data = pd.DataFrame([years, GSP, GDD, GSTmin, GSTmax, frost, summer, HWI, CWI]).T
 data['YEAR'] = data.YEAR.astype(int)
 print(data)
 
