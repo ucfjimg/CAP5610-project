00b6cf25ff8220b58a0072d7af98486ccfe33aa6
Incapamentum <gdiaz0710@gmail.com>
2021-04-10 11:31:16 -0400

    OG data set now in range of 1940-2015. Script now filters out even more of it to a new dataset.

--- data/agri/frontiers/process_data.py
+++ data/agri/frontiers/process_data.py
@@ -14,8 +14,24 @@ EMPTY_COLUMNS = ["Week Ending", "State ANSI", "Ag District Code", "County ANSI",
 # Open the data
 cg_df = pd.read_csv(FILE_NAME)
 
+# Removing unnecessary columns
 for col in EMPTY_COLUMNS:
     if col in cg_df.columns:
         cg_df = cg_df.drop(columns=col, axis=1)
 
-cg_df.to_csv(PROCESSED_FILE, index=False)
+# Filtering out data that contains a specific metric
+yield_df = cg_df[cg_df["Data Item"].str.contains("BU / ACRE")]
+
+# Figuring out the commodity counts
+commodity_count = yield_df["Commodity"].value_counts()
+
+# Top two results equal to one another?
+if (commodity_count.iloc[0] == commodity_count.iloc[1]):
+    commodityOne = commodity_count.index[0]
+    commodityTwo = commodity_count.index[1]
+
+# Creating new dataframe
+crop_df = yield_df[yield_df["Commodity"].isin([commodityOne, commodityTwo])]
+
+# Writing
+crop_df.to_csv(PROCESSED_FILE, index=False)
